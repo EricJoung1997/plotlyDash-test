1dba89f17811e91f2080ececeacfb8b8af383a86
Eric Joung <44860515+EricJoung1997@users.noreply.github.com>
2023-03-17 16:26:45 +0800

    Create requirements.txt

--- app.py
+++ app.py
@@ -3,28 +3,36 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
+app.title = 'ZYJtest'
 application = app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
-        This is Dash running on Azure App Service.
+        This is Dash running on Codespace.
     '''),
 
     dcc.Graph(
         id='example-graph',
+        config={
+                    'displayModeBar': True,
+                    'displaylogo': False,                                       
+                    'modeBarButtonsToRemove': ['zoom2d', 'hoverCompareCartesian', 'hoverClosestCartesian', 'toggleSpikelines']
+                },
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'This is a test'
             }
         }
     )
 ])
+
+
 
 if __name__ == '__main__':
     application.run(debug=True)
